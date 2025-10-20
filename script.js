ddb266482d4500cde393f73eb0100d86639cba1a
Mirza-Anjum <mirzakhan8456@gmail.com>
2025-10-20 22:45:19 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,11 @@
+function wings() {
+  alert(
+    "✨Tip from Wings of Fire: “Dream, dream, dream. Dreams transform into thoughts and thoughts result in action.”"
+  );
+}
+
+function confess() {
+  alert(
+    "💖Tip from Confess: “There are people you meet that you get to keep. And then there are people you meet that you have to let go.”"
+  );
+}
